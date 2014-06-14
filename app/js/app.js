f0f35592fd7fcaa3ebbdb020ef53f89e44d6ba4f
Salih Kardan <kardan38@gmail.com>
2014-06-15 01:16:24 +0300

    restangular support is added

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,13 @@
 'use strict';
 
+angular.module('myApp', ['ngRoute', 'restangular', 'myApp.controllers', 'myApp.services', 'myApp.directives', 'myApp.filters']).
+  
+	config(function($routeProvider, RestangularProvider) {
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+		$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'ListCtrl'});
+	  	$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+	  	$routeProvider.otherwise({redirectTo: '/view1'});
+		//RestangularProvider.setBaseUrl('http://10.0.3.246:8080/');
+		RestangularProvider.setBaseUrl('http://filltext.com/');
+
+	});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+	.controller('MyCtrl1', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  	}])
+  	.controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  	}])
+  	.controller("ListCtrl",['$scope','Restangular',function($scope,r){
+  		// r.all('?rows=5&id={index}&email={email}&username={username}&password={randomString|5}&pretty=true').getList()
+		r.all('students').getList()
+		.then(function(data) {
+			$scope.people = data;
+		});
+	}]);
